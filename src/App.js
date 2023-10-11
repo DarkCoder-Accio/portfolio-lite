ccc4b7878ff73525c3568a0ec5493459522ec38d
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-10-11 21:57:40 +0530

    portfolio-lite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"; 
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Section from "./Components/Section";
+// import "./style.css"
+import sectionData from "./data/sectionData";
+
+
+
+
+const App = () => {
+
+
+
+     return(
+        <div>
+            <Navbar />
+            <About />
+
+          {
+             sectionData.map(value => (
+                  <Section 
+                     x={value.title}
+                     y={value.des}
+                  />
+             ))
+
+          }
+
+        </div>
+     )
 }
 
 export default App;
+
+
+// ifelse => ternory operator 
+
+// loops => higher order function => map, filter, reduce
